7475fb7183ed38afc2ddc2dede4754f1417ef904
andrey <tsariukandreyjs@gmail.com>
2018-02-27 23:37:50 +0200

    fix for reloading

--- dist/server.js
+++ dist/server.js
@@ -4,5 +4,8 @@ var serveStatic = require('serve-static')
 app = express()
 app.use(serveStatic(__dirname))
 var port = process.env.PORT || 5000
-app.listen(port)
+app.listen(port);
+app.use((req, res) => {
+    res.sendFile('./index.html', { root: __dirname });
+});
 console.log('server started ' + port)
